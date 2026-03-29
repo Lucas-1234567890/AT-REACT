ab412d258fc791f581ce45a928a9b6ee58c04f8e
Lucas Amorim <lucas.amorim.porciuncula@gmail.com>
2026-03-29 08:18:46 -0300

    tarefa 2: useState e useEffect para buscar clubes.json

--- src/components/ClubeLista.jsx
+++ src/components/ClubeLista.jsx
@@ -1,8 +1,22 @@
+import { useState, useEffect } from 'react';
+
 function ClubeLista() {
+  const [clubes, setClubes] = useState([]);
+
+  useEffect(() => {
+    fetch('/clubes.json')
+      .then((res) => res.json())
+      .then((data) => setClubes(data));
+  }, []);
+
   return (
     <div>
       <h1>Clubes de Leitura</h1>
-      <ul></ul>
+      <ul>
+        {clubes.map((clube) => (
+          <li key={clube.id}>{clube.nome}</li>
+        ))}
+      </ul>
     </div>
   );
 }
