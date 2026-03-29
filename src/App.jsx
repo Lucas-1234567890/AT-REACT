c7791cab247e5d6180e414b96605974e1fb918be
Lucas Amorim <lucas.amorim.porciuncula@gmail.com>
2026-03-29 08:27:15 -0300

    tarefa 5: configuracao do React Router com rotas index e /adicionar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import ClubeLista from './components/ClubeLista';
+import NovoClube from './components/NovoClube';
 
 function App() {
-  return <ClubeLista />;
+  return (
+    <Routes>
+      <Route index element={<ClubeLista />} />
+      <Route path="/adicionar" element={<NovoClube />} />
+    </Routes>
+  );
 }
 
 export default App;
